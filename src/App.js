ac8fed75c54c11822de7162ade229fa94f8a4770
Sal Tamay <sal.tamay@outlook.com>
2020-06-30 15:14:22 -0400

    [WIS] Header with initial logo design

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from './lib/theme.js';
+
+import { Header } from './sections';
 
 function App() {
-  return <div className='App'>The Underground Dance Centre</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <div className='App'>
+        <Header />
+        The Underground Dance Centre
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
